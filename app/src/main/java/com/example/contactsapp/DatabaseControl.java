d0c7577701018502e4fb196a8a2c46122a95adf8
Temp Proj <e>
2023-04-18 12:07:41 -0400

    Intial Commit

--- app/src/main/java/com/example/contactsapp/DatabaseControl.java
+++ app/src/main/java/com/example/contactsapp/DatabaseControl.java
@@ -5,7 +5,9 @@ import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 
-public class DatabaseControl {
+import java.util.ArrayList;
+
+public class  DatabaseControl {
 
     SQLiteDatabase database;
     DatabaseHelper helper;
@@ -22,21 +24,60 @@ public class DatabaseControl {
         helper.close();
     }
 
-    public boolean insert(String name, String state) {
+    public boolean insert(String name, String relationship, Integer age) {
         ContentValues values = new ContentValues();
         values.put("name", name);
-        values.put("state", state);
+        values.put("relationship", relationship);
+        values.put("age", age);
         return database.insert("contact", null, values) > 0;
     }
 
-    public String getState(String name) {
-        String query = "select state from contact where name=\""+name+"\"";
+    public void delete(String n) {
+        database.delete("contact","name =\""+n+"\"",null);
+    }
+
+    public int getAge(String name) {
+        String query = "select age from contact where name=\""+name+"\"";
         Cursor cursor = database.rawQuery(query, null);
         cursor.moveToFirst();
-        String state = cursor.getString(0);
+        int age = cursor.getInt(0);
         cursor.close();
-        return state;
+        return age;
     }
+
+    public String getRelationship(String name) {
+        String query = "select relationship from contact where name=\""+name+"\"";
+        Cursor cursor = database.rawQuery(query, null);
+        cursor.moveToFirst();
+        String relationship = cursor.getString(0);
+        cursor.close();
+        return relationship;
+    }
+
+    public String getName(String name) {
+        String query = "select name from contact where name=\""+name+"\"";
+        Cursor cursor = database.rawQuery(query, null);
+        cursor.moveToFirst();
+        String Name = cursor.getString(0);
+        cursor.close();
+        return Name;
+    }
+
+    public String[] getAllRelationshipsArray () {
+        String query = "select relationship from contact";
+        Cursor cursor = database.rawQuery(query, null);
+        cursor.moveToFirst();
+        ArrayList<String> list = new ArrayList<String>();
+        while(!cursor.isAfterLast()) {
+            String name = cursor.getString(0);
+            list.add(name);
+            cursor.moveToNext();
+        }
+        cursor.close();
+        String[] array = new String[list.size()];
+        return list.toArray(array);
+    }
+
 
 }
 
--- app/src/main/java/com/example/contactsapp/MainActivity.java
+++ app/src/main/java/com/example/contactsapp/MainActivity.java
@@ -1,12 +1,15 @@
 package com.example.contactsapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListAdapter;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -14,10 +17,14 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
 
     EditText nameEdit;
+    EditText ageEdit;
     Spinner spinner;
     Button addButton;
     Button getButton;
     DatabaseControl control;
+    RecyclerView list;
+    Button deletebutton;
+    TextView resultView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,29 +34,81 @@ public class MainActivity extends AppCompatActivity {
         control = new DatabaseControl(this);
 
         nameEdit = findViewById(R.id.nameEdit);
+        ageEdit = findViewById(R.id.ageEdit);
         spinner = findViewById(R.id.spinner);
         addButton = findViewById(R.id.addButton);
         getButton = findViewById(R.id.getButton);
+        deletebutton = findViewById(R.id.deletebutton);
+        resultView = findViewById(R.id.resultView);
+        list = findViewById(R.id.list);
+
+        deletebutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick (View view){
+                String name = nameEdit.getText().toString();
+                control.open();
+                control.delete(name);
+                control.close();
+                onResume();
+            }
+        });
+
 
         addButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 String name = nameEdit.getText().toString();
-                String state = ((TextView) spinner.getSelectedView()).getText().toString();
+                String relationship = ((TextView) spinner.getSelectedView()).getText().toString();
+                Integer age = Integer.parseInt(ageEdit.getText().toString());
                 control.open();
-                boolean itWorked = control.insert(name, state);
+                boolean itWorked = control.insert(name, relationship, age);
                 control.close();
                 if (itWorked)
-                    Toast.makeText(getApplicationContext(), "Added "+name+" "+state, Toast.LENGTH_SHORT).show();
+                    Toast.makeText(getApplicationContext(), "Added "+name+" "+relationship, Toast.LENGTH_SHORT).show();
                 else
-                    Toast.makeText(getApplicationContext(), "FAILED "+name+" "+state, Toast.LENGTH_SHORT).show();
+                    Toast.makeText(getApplicationContext(), "FAILED "+name+" "+relationship, Toast.LENGTH_SHORT).show();
             }
         });
 
+        getButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String name = nameEdit.getText().toString();
+                control.open();
+                int year = control.getAge(name);
+                String kin = control.getRelationship(name);
+                String Name = control.getName(name);
+                control.close();
+                resultView.setText(Name+" "+kin+" "+year+"yr old");
+            }
+        });
+
+
+
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
-                R.array.states, android.R.layout.simple_spinner_item);
+                R.array.relationship, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(adapter);
+    }
+    @Override
+    public void onResume() {
+        super.onResume();
+        control.open();
+        String[] dataset = control.getAllRelationshipsArray();
+        control.close();
+        list.setLayoutManager(new LinearLayoutManager(this));
+        CustomAdapter adapter = new CustomAdapter(dataset);
+        adapter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                CustomAdapter.ViewHolder viewHolder = (CustomAdapter.ViewHolder) view.getTag();
+                TextView textView = viewHolder.getTextView();
+                resultView.setText(textView.getText().toString());
+            }
+        });
+
+        list.setLayoutManager(new LinearLayoutManager(this));
+
     }
 }
 
